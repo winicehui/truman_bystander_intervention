a5bac0edc3eb589de589e202b302f22e42dd32b9
Winice Hui <35130094+winicehui@users.noreply.github.com>
2026-02-25 11:04:59 -0500

    Initial commit

---
+++ models/Notification.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const notificationSchema = new mongoose.Schema({
+    actor: { type: Schema.ObjectId, ref: 'Actor' }, // Indicates which Actor does the action
+    notificationType: String, // 3 types of action/notifications: 'like', 'read', 'reply'
+    time: Number, // Indicates when the action occurs relative to the time of the user's comment or post, in milliseconds
+
+    userPostID: Number, // Indicates which user post this action responds to (0,1,2....n)
+    userReplyID: Number, // Indicates which user reply this action responds to (0,1,2....n)
+    replyBody: { type: String, default: '', trim: true }, // Text(body) of the actor's reply
+
+    class: {
+        type: String,
+            default: '', trim: true
+    },
+    condition: { type: String, default: '', trim: true }, // For experimental use (If blank/null, this comment is shown to all users. If defined, this comment is shown only to users with the same value for their experimental condition)
+
+}, { timestamps: true });
+
+const Notification = mongoose.model('Notification', notificationSchema);
+module.exports = Notification;
